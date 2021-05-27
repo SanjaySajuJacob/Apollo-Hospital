22ed95b69da1fa9783b46e6aacb206b2e7ef1a43
Jayanand Jayan <jayanand.jayan2020@vitstudent.ac.in>
2021-05-27 12:27:58 +0530

    Finished Emplogin and Patlogin

--- models.py
+++ models.py
@@ -8,11 +8,15 @@ class Employee(models.Model):
     emp_name = models.CharField(max_length = 100)
     designation = models.CharField(max_length = 100)
     department = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.emp_id
 
 class Accomodation(models.Model):
     room_type = models.CharField(max_length = 100, primary_key = True)
     no_of_beds_left = models.IntegerField(default = 100)
     cost = models.IntegerField(default = 10000)
+    def __str__(self):
+        return self.room_type
 
 class Patients(models.Model):
     patient_id = models.CharField(max_length = 100, primary_key = True)
@@ -21,23 +25,33 @@ class Patients(models.Model):
     age = models.IntegerField(default = 0)
     phone_no = models.IntegerField(default = 9999999999)
     profession = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.patient_id
 
 class EmpFinance(models.Model):
     emp_id = models.ForeignKey(Employee, on_delete = models.CASCADE)
     salary = models.IntegerField(default = 10000)
+    def __str__(self):
+        return self.emp_id
 
 class PatFinance(models.Model):
     patient_id = models.ForeignKey(Patients, on_delete = models.CASCADE)
     payment_method = models.CharField(max_length = 100)
     amount_paid = models.IntegerField(default = 0)
     date = models.DateTimeField('payment_date')
+    def __str__(self):
+        return self.patient_id
 
 class Vaccines(models.Model):
     vaccine_name = models.CharField(max_length = 100, primary_key = True)
     vaccine_stock = models.IntegerField(default = 100)
     vaccine_cost = models.IntegerField(default = 1000)
+    def __str__(self):
+        return self.vaccine_name
 
 class CovApply(models.Model):
     patient_id = models.ForeignKey(Patients, on_delete = models.CASCADE)
     vaccine_name = models.ForeignKey(Vaccines, on_delete = models.CASCADE)
     covid_test_result = models.CharField(max_length = 10)
+    def __str__(self):
+        return self.patient_id
--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
-from .models import Employee, Patients
+from .models import Employee, Patients, Accomodation
 # Create your views here.
 
 def homepage(request):
@@ -12,38 +12,28 @@ def homepage(request):
 
 def emploginpage(request):
     if request.method == "POST":
-        if request.POST.get('EmpID'):
-            post = Employee(emp_id=request.POST.get('EmpID'))
+        if request.POST.get('EmpID') and request.POST.get('Full Name') and request.POST.get('Designation') and request.POST.get('Department'):
+            post = Employee()
+            post.emp_id = request.POST.get('EmpID')
+            post.emp_name = request.POST.get('Full Name')
+            post.designation = request.POST.get('Designation')
+            post.department = request.POST.get('Department')
             post.save()
-            return render(request, 'apollo/emploginpage.html')
-        if request.POST.get('Full Name'):
-            post = Employee(emp_name=request.POST.get('Full Name'))
-            post.save()
-            return render(request, 'apollo/emploginpage.html')
+            return render(request, 'apollo/emplogin.html')
     else:
-        return render(request, 'apollo/emploginpage.html')
+        return render(request, 'apollo/emplogin.html')
 
 def patloginpage(request):
     if request.method == "POST":
-        if request.POST.get('PatID'):
-            post = Employee(emp_id=request.POST.get('PatID'))
+        if request.POST.get('PatID') and request.POST.get('Patient Name') and request.POST.get('Age') and request.POST.get('Ph No') and request.POST.get('Profession'):
+            post = Patients()
+            post.patient_id = request.POST.get('PatID')
+            post.patient_name = request.POST.get('Patient Name')
+            post.age = request.POST.get('Age')
+            post.phone_no = request.POST.get('Ph No')
+            post.profession = request.POST.get('Profession')
+            post.room_type = Accomodation(request.POST.get('Room'))
             post.save()
-            return render(request, 'apollo/patloginpage.html')
-        if request.POST.get('Patient Name'):
-            post = Employee(emp_name=request.POST.get('Patient Name'))
-            post.save()
-            return render(request, 'apollo/patloginpage.html')
-        if request.POST.get('Age'):
-            post = Employee(emp_name=request.POST.get('Age'))
-            post.save()
-            return render(request, 'apollo/patloginpage.html')
-        if request.POST.get('Ph No'):
-            post = Employee(emp_name=request.POST.get('Ph No'))
-            post.save()
-            return render(request, 'apollo/patloginpage.html')
-        if request.POST.get('Profession'):
-            post = Employee(emp_name=request.POST.get('Profession'))
-            post.save()
-            return render(request, 'apollo/patloginpage.html')
+            return render(request, 'apollo/patlogin.html')
     else:
-        return render(request, 'apollo/patloginpage.html')
+        return render(request, 'apollo/patlogin.html')
